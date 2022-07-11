4d8ac153a086f3ce47a78ee8e9b33951243025af
Yousopp <yousephagueye@gmail.com>
2022-07-11 19:44:41 -0400

    updated js sheet

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-let updateTime = function() {
-    document.getElementById('currentDay').innerHTML=moment().format("dddd, MMM Do, h:mm:ss a"
-    );
-};
+function updateTime() {
+    let today = moment();
 
-setInterval(
-    updateTime, 1000
-);
+    $("#currentDay").text(today.format("dddd, MMM Do YYYY, h:mm:ss"));
+
+    let rightNow = moment().format("kk");
+    for (let i = 0; i < theScheduleElArray.length; i++) {
+        theScheduleElArray[i].removeClsss('future past present');
+
+        if (rightNow > theScheduleElArray[i].data("hour")) {
+            theScheduleElArray[i].addClass("past");
+
+        } else if (rightNow === theScheduleElArray[i].attr("data-hour")) {
+            theScheduleElArray[i].addClass("present");
+
+        } else {
+            theScheduleElArray[i].addClass("future");
+        }
+    }
+}
